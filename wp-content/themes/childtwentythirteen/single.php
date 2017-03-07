8731d1fa1226520e309d04cd44995c58d6bbf23d
Kerri <kerridale.smith@gmail.com>
2017-03-07 18:14:56 -0500

    Updated single.php for 2013 child theme

--- wp-content/themes/childtwentythirteen/single.php
+++ wp-content/themes/childtwentythirteen/single.php
@@ -1,6 +1,10 @@
 <?php
 /**
- * Template Name: sidebar
+ * The template for displaying all single posts
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
  */
 
 get_header(); ?>
@@ -11,28 +15,10 @@ get_header(); ?>
 			<?php /* The loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<header class="entry-header">
-						<?php if ( has_post_thumbnail() && ! post_password_required() ) : ?>
-						<div class="entry-thumbnail">
-							<?php the_post_thumbnail(); ?>
-						</div>
-						<?php endif; ?>
-
-						<h1 class="entry-title"><?php the_title(); ?></h1>
-					</header><!-- .entry-header -->
-
-					<div class="entry-content">
-						<?php the_content(); ?>
-						<?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentythirteen' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
-					</div><!-- .entry-content -->
-
-					<footer class="entry-meta">
-						<?php edit_post_link( __( 'Edit', 'twentythirteen' ), '<span class="edit-link">', '</span>' ); ?>
-					</footer><!-- .entry-meta -->
-				</article><!-- #post -->
-
+				<?php get_template_part( 'content', get_post_format() ); ?>
+				<?php twentythirteen_post_nav(); ?>
 				<?php comments_template(); ?>
+
 			<?php endwhile; ?>
 
 		</div><!-- #content -->
